bb78b86b4422f846833c7a2b093fedf557a5e911
HoaAng911 <mayanh279@gmail.com>
2024-12-23 15:38:21 +0700

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",  // Đối với các dự án thuần HTML
+    "./src/**/*.{js,jsx,ts,tsx}",  // Đối với các dự án React hoặc Vite
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
